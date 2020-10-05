2c7cdfc5e2739b4bf732fe539ecc69676d7203f8
Geralt from Revia <56021873+gveenblade@users.noreply.github.com>
2020-10-06 02:05:10 +0600

    Update d.cpp

--- d.cpp
+++ d.cpp
@@ -8,24 +8,5 @@ int main()
     int y = ((b>>k) & 1);
     if(x==y) cout<<"Money was found";
     else cout<<"Where is the money Lebowski?";
-    
-
-
-    //1 01 & 1
-    //2 01 & 1
-    
-    //2  001  
-    //5  001 
-
-    //4  0001
-    //2  0001
-
-
-    //4  0010 & 1 int x = 0 
-    //5  0101 & 1 int y = 1
-
-
-    // 0 & 1 = 0
-    // 1 & 1 = 1
     return 0;
 }
