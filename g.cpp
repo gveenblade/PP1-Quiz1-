dfcb3fce96c2a02717ea4f1c9f2bbe7e4c97311e
Geralt from Revia <56021873+gveenblade@users.noreply.github.com>
2020-10-06 02:02:07 +0600

    Update g.cpp
    Hacktoberfest changes

--- g.cpp
+++ g.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 int main()
 {
-    int n,a,b, day = 0, cur = 0;
+    int n, a, b, day = 0, cur = 0;
     bool isClimbing = true;
-    cin>>n>>a>>b;
-    while(cur<n)
+    cin>> n >> a >> b;
+    while( cur < n )
     {
         day++;
-        cur+=a;
-        if(cur>=n) {
+        cur += a;
+        if(cur >= n) {
             break;
         }
-        cur-=b;
+        cur -= b;
         if(cur <= 0) {
             isClimbing = false;
             break;
         }
     }
-    if(isClimbing) cout<<day;
-    else cout<<"NO";
+    if( isClimbing ) cout << day;
+    else cout << "NO";
     return 0;
 }
